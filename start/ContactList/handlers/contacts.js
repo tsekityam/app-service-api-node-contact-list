3c33dd70b6996b4af1911832e6fb277a425a1501
Tse Kit Yam <me@kytse.com>
2018-01-13 21:30:04 +0800

    Customize the project code

--- start/ContactList/handlers/contacts.js
+++ start/ContactList/handlers/contacts.js
@@ -1,29 +1,9 @@
 'use strict';
-var dataProvider = require('../data/contacts.js');
-/**
- * Operations on /contacts
- */
+
+var repository = require('../lib/contactRepository');
+
 module.exports = {
-    /**
-     * summary: 
-     * description: 
-     * parameters: 
-     * produces: application/json, text/json
-     * responses: 200
-     */
-    get: function contacts_get(req, res, next) {
-        /**
-         * Get the data for response 200
-         * For response `default` status 200 is used.
-         */
-        var status = 200;
-        var provider = dataProvider['get']['200'];
-        provider(req, res, function (err, data) {
-            if (err) {
-                next(err);
-                return;
-            }
-            res.status(status).send(data && data.responses);
-        });
+    get: function contacts_get(req, res) {
+        res.json(repository.all())
     }
 };
--- start/ContactList/server.js
+++ start/ContactList/server.js
@@ -1,28 +1,33 @@
 'use strict';
 
-var Http = require('http');
-var Express = require('express');
-var BodyParser = require('body-parser');
-var Swaggerize = require('swaggerize-express');
-var Path = require('path');
+var port = process.env.PORT || 8000;
 
-var App = Express();
+var http = require('http');
+var express = require('express');
+var bodyParser = require('body-parser');
+var swaggerize = require('swaggerize-express');
+var swaggerUi = require('swaggerize-ui');
+var path = require('path');
+var fs = require("fs");
 
-var Server = Http.createServer(App);
+fs.existsSync = fs.existsSync || require('path').existsSync;
 
-App.use(BodyParser.json());
-App.use(BodyParser.urlencoded({
-    extended: true
+var app = express();
+
+var server = http.createServer(app);
+
+app.use(bodyParser.json());
+
+app.use(swaggerize({
+    api: path.resolve('./config/swagger.json'),
+    handlers: path.resolve('./handlers'),
+    docspath: '/swagger'
 }));
 
-App.use(Swaggerize({
-    api: Path.resolve('./config/swagger.json'),
-    handlers: Path.resolve('./handlers')
+// change four
+app.use('/docs', swaggerUi({
+    docs: '/swagger'
 }));
 
-Server.listen(8000, function () {
-    App.swagger.api.host = this.address().address + ':' + this.address().port;
-    /* eslint-disable no-console */
-    console.log('App running on %s:%d', this.address().address, this.address().port);
-    /* eslint-disable no-console */
+server.listen(port, function () {
 });
